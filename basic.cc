eb715a46ef586a7b636fa37774b9a61ce55bbc52
Wink Saville <wink@saville.com>
2018-08-22 14:04:38 -0700

    Add Threads and ThreadRange

--- basic.cc
+++ basic.cc
@@ -17,5 +17,7 @@ static void BM_StringCopy(benchmark::State& state) {
     std::string copy(x);
 }
 BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_StringCopy)->Threads(6);
+BENCHMARK(BM_StringCopy)->ThreadRange(1, 1024);
 
 BENCHMARK_MAIN();
